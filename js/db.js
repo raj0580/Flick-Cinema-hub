3f3ea6fddad8bf9342f6c2334d14fb3a1fd73a4f
raj0580 <duckdikcow@gmail.com>
2025-07-11 14:42:51 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,47 +1,34 @@
 import { db } from './firebase-config.js';
-import { 
-    collection, 
-    getDocs, 
-    getDoc, 
-    addDoc, 
-    updateDoc, 
-    deleteDoc, 
-    doc,
-    orderBy,
-    query 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc, orderBy, query } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const moviesCollection = collection(db, 'movies');
 const requestsCollection = collection(db, 'movie_requests');
+const adsCollection = collection(db, 'ads');
 
 export const getMovies = async () => {
     const snapshot = await getDocs(moviesCollection);
     return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
-
 export const getMovieById = async (id) => {
     const docRef = doc(db, 'movies', id);
     const docSnap = await getDoc(docRef);
     return docSnap.exists() ? { id: docSnap.id, ...docSnap.data() } : null;
 };
-
 export const addMovie = async (movieData) => await addDoc(moviesCollection, movieData);
-
 export const updateMovie = async (id, movieData) => await updateDoc(doc(db, 'movies', id), movieData);
-
 export const deleteMovie = async (id) => await deleteDoc(doc(db, 'movies', id));
 
-// --- NEW Movie Request Functions ---
-export const addMovieRequest = async (requestData) => {
-    return await addDoc(requestsCollection, requestData);
-};
-
+export const addMovieRequest = async (requestData) => await addDoc(requestsCollection, requestData);
 export const getMovieRequests = async () => {
     const q = query(requestsCollection, orderBy('requestedAt', 'desc'));
     const snapshot = await getDocs(q);
     return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
+export const deleteMovieRequest = async (id) => await deleteDoc(doc(db, 'movie_requests', id));
 
-export const deleteMovieRequest = async (id) => {
-    return await deleteDoc(doc(db, 'movie_requests', id));
+export const getAds = async () => {
+    const snapshot = await getDocs(adsCollection);
+    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
+export const addAd = async (adData) => await addDoc(adsCollection, adData);
+export const deleteAd = async (id) => await deleteDoc(doc(db, 'ads', id));
